bfc0e6ae643c8a50e96e9f44c0d3bcc3586d721d
Amirali Esfandiari <amiralinull@gmail.com>
2020-08-17 21:16:20 +0430

    fix done/undone and list

--- twodo/cli.py
+++ twodo/cli.py
@@ -4,6 +4,8 @@ import fire
 class Twodo:
     def __init__(self):
         self.file = os.path.join(os.path.expanduser('~'), '.twodo.txt')
+        self.undone_sign = '[ ]'
+        self.done_sign = '[x]'
 
         if not os.path.exists(self.file):
             with open(self.file, 'w') as f:
@@ -17,53 +19,60 @@ class Twodo:
 
     def clean(self):
         with open(self.file, 'r') as f:
-            todos = f.read().split('\n')
+            tasks = f.read().split('\n')
         
-        for i in range(len(todos)-1):
-            if todos[i][0] == '+':
-                del todos[i]
+        for i in range(len(tasks)-1):
+            if tasks[i][0] == '+':
+                del tasks[i]
 
         with open(self.file, 'w') as f:
-            f.write('\n'.join(todos))
+            f.write('\n'.join(tasks))
 
     def delete(self, *tids):
         pass
 
     def done(self, *tids):
         with open(self.file, 'r') as f:
-            todos = f.read().split('\n')
+            tasks = f.read().split('\n')
 
         for tid in tids:
             tid = int(str(tid).lstrip('0'))
-            if tid > len(todos):
+            if tid > len(tasks):
                 print("There isn't this todo index")
                 return
 
-            todos[tid] = '+' + todos[tid][1:]
+            tasks[tid-1] = '+' + tasks[tid-1][1:]
 
         with open(self.file, 'w') as f:
-            f.write('\n'.join(todos))
+            f.write('\n'.join(tasks))
 
     def list(self):
-        pass
+        with open(self.file, 'r') as f:
+            tasks = f.read().split('\n')
+
+        for i in range(len(tasks)-1):
+            if tasks[i][0] == '-':
+                print(self.undone_sign + ' ' + str(i+1).zfill(3) + ": " + tasks[i][1:])
+            else:
+                print(self.done_sign + ' ' + str(i+1).zfill(3) + ": " + tasks[i][1:])
 
     def sort(self):
         pass
 
     def undone(self, *tids):
         with open(self.file, 'r') as f:
-            todos = f.read().split('\n')
+            tasks = f.read().split('\n')
 
         for tid in tids:
             tid = int(str(tid).lstrip('0'))
-            if tid > len(todos):
+            if tid > len(tasks):
                 print("There isn't this todo index")
                 return
 
-            todos[tid] = '-' + todos[tid][1:]
+            tasks[tid-1] = '-' + tasks[tid-1][1:]
 
         with open(self.file, 'w') as f:
-            f.write('\n'.join(todos))
+            f.write('\n'.join(tasks))
 
 def main():
     fire.Fire(Twodo)
