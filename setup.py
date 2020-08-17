ba38cba18d6f8f444f10c2d88dc7fb05d8b172ed
Amirali Esfandiari <amiralinull@gmail.com>
2020-08-17 23:35:26 +0430

    fix setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
+with open('readme', 'r') as f:
+    long_description = f.read()
+
 setup(
     name = 'twodo',
-    version = '1',
+    version = '1.1',
     description = 'Yet another simple todo cli in Python',
+    long_description = long_description,
     author = 'Amirali Esfandiari',
     author_email = 'amiralinull@gmail.com',
-    url = 'https://github.com/realamirali/twodo',
+    url = 'https://github.com/realamirali/2do',
     py_modules = ['twodo'],
     install_requires = ['fire'],
     packages = ['twodo'],
