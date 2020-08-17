33d85f9b5309d875249d0205f034d693c75b1323
Amirali Esfandiari <amiralinull@gmail.com>
2020-08-17 20:31:31 +0430

    add clean

--- twodo/cli.py
+++ twodo/cli.py
@@ -16,7 +16,15 @@ class Twodo:
             f.writelines('-' + line + '\n')
 
     def clean(self):
-        pass
+        with open(self.file, 'r') as f:
+            todos = f.read().split('\n')
+        
+        for i in range(len(todos)-1):
+            if todos[i][0] == '+':
+                del todos[i]
+
+        with open(self.file, 'w') as f:
+            f.write('\n'.join(todos))
 
     def delete(self, *tids):
         pass
